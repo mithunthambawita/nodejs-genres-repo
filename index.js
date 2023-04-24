67b29e20791c5c3c41b5fb02df28652033e545bc
mithun thambawita <mithunthambavita@gmail.com>
2023-04-24 22:36:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+const express =  require('express');
+const app = express();
+const genres = require('./routes/genres');
+
+app.use(express.json());
+
+app.use('/api/genres',genres);
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>console.log(`Listenng on port ${port}...`));
